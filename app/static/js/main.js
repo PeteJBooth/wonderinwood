1e6cf382b06ad3d078354a318648c0c38962b2a2
Pete Booth <petebooth@Petes-MBP-3.home>
2015-07-05 16:58:30 +0100

    Fixing the category menu

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
-    //$('.overlay').hide();
     $('.nav-list .child-container').slideUp(0);
     $('.nav-list .nav-arrow').addClass('closed');
+    
+    //re-open any current menu items and their parents
+    $active = $('.nav-list .link_container.current');
+    while(true){
+        $active.parent().slideDown(0);
+    	if ($active.parent('.child-container').length >0){
+    		$active.parent('.child-container').slideDown(0);
+    		$active = $('#cat-' + $active.parent().data('child-of'));
+    		continue;
+    	}
+    	break;
+
+    }
     $('.nav-list .nav-arrow').click(function(e){
         e.preventDefault();
         $(this).toggleClass('closed');
-    	$(this).parent().siblings('.child-container').slideToggle(400);
+    	$(this).parent().next('.child-container').slideToggle(400);
     });
 
 })
