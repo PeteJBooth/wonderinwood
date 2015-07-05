204b6fac827c489edbd7d121791be1c9b179543d
vagrant <vagrant@vagrant-ubuntu-server.(none)>
2015-07-05 14:15:22 +0100

    Starting to sort out the product listing styling

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     //$('.overlay').hide();
-
+    $('.nav-list .child-container').slideUp(0);
+    $('.nav-list .nav-arrow').addClass('closed');
+    $('.nav-list .nav-arrow').click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('closed');
+    	$(this).parent().siblings('.child-container').slideToggle(400);
+    });
 
 })
